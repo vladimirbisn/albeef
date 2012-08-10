a3e90a70716d0f746b0745b7bc7d197d37a0db90
Vladimir Aderikhin <v.aderikhin@roundlake.ru>
2012-08-11 02:09:02 +0400

    postgreSQL

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,7 +1,13 @@
+require 'date'
+
 module NewsHelper
   def formatDate(date)
+    if date.nil?
+      date = DateTime.new(1111,1,1,1,1,1)
+    end
     date.strftime("%m.%d.%y")
   end
+
   def format483(body)
     body[0..415]+'...'
   end

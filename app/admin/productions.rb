69c74f69eed8ab6bd4683fa5f601377d33684f44
Vladimir Aderikhin <v.aderikhin@roundlake.ru>
2013-06-06 13:40:23 +0400

    adctive_admin_editor switched to textarea on prod page

--- app/admin/productions.rb
+++ app/admin/productions.rb
@@ -31,7 +31,7 @@ ActiveAdmin.register Production do
       f.input :dish_title, label: 'Название блюда'
       f.input :top_description, label: 'Описание отруба', as: :html_editor
       f.input :ingredients, label: 'Ингридиенты', as: :html_editor
-      f.input :preparation, label: 'Приготовление', as: :html_editor
+      f.input :preparation, label: 'Приготовление', as: :text
       f.input :part_id, label: 'Номер отруба'
       f.input :part_img_url1, label: 'Изображение отруба1', hint: f.object.part_img_url1 ? f.template.image_tag(f.object.part_img_url1, width: 300) : f.template.content_tag(:span, "Пока нет картинки")
       f.input :part_img_url2, label: 'Изображение отруба2', hint: f.object.part_img_url2 ? f.template.image_tag(f.object.part_img_url2, width: 300) : f.template.content_tag(:span, "Пока нет картинки")
